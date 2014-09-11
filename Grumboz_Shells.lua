ff0b7eece96801f1c278df4059850d6c63881a32
Grumbo <blackwolfsden@hotmail.com>
2014-09-10 17:26:53 -0700

    Update Grumboz_Shells.lua
    rofl and again allways forgettin that 1 random thing

--- Grumboz_Shells.lua
+++ Grumboz_Shells.lua
@@ -10,6 +10,18 @@ local Shells = {{"Red"},{"Green"},{"Blue"},} -- its dynamic so add as many color
 
 -- DO NOT EDIT BELOW HERE --
 
+local function GetItemNameById(id)
+local err = "ERROR GetItemById() name value is nil(Item "..id.." May not exist in database)"
+local search = WorldDBQuery("SELECT `name` FROM `item_template` WHERE `entry` = '"..id.."';");
+
+	if(search)then
+		local itemname = search:GetString(0)
+		return(itemname)
+	else
+		error(err)
+	end
+end
+
 local currency_name = GetItemNameById(currency)
 
 local function ShuffleShells(player, unit, guid)
